10197449e0c0c5f532576fcf69a8e3cc8c7ce2b9
VISTALL <vistall.valeriy@gmail.com>
2015-08-11 09:38:00 +0300

    consulo installation

---
+++ src/main/java/org/mustbe/consulo/jenkins/installation/ConsuloInstallation.java
@@ -0,0 +1,63 @@
+package org.mustbe.consulo.jenkins.installation;
+
+import java.io.File;
+import java.util.List;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+import hudson.Extension;
+import hudson.tools.ToolDescriptor;
+import hudson.tools.ToolInstallation;
+import hudson.tools.ToolProperty;
+import hudson.util.FormValidation;
+import jenkins.model.Jenkins;
+
+/**
+ * @author VISTALL
+ * @since 11.08.2015
+ */
+public class ConsuloInstallation extends ToolInstallation
+{
+	@Extension
+	public static class DescriptorImpl extends ToolDescriptor<ConsuloInstallation>
+	{
+		@Override
+		public String getDisplayName()
+		{
+			return "Consulo";
+		}
+
+		@Override
+		public FormValidation doCheckHome(@QueryParameter File value)
+		{
+			if(!Jenkins.getInstance().hasPermission(Jenkins.ADMINISTER))
+			{
+				return FormValidation.ok();
+			}
+
+			if(value.getPath().equals(""))
+			{
+				return FormValidation.ok();
+			}
+
+			if(!value.isDirectory())
+			{
+				return FormValidation.error("Not Consulo Directory");
+			}
+
+			File ideaJar = new File(value, "lib/idea.jar");
+			if(!ideaJar.exists())
+			{
+				return FormValidation.error("Not Consulo Directory");
+			}
+
+			return FormValidation.ok();
+		}
+	}
+
+	@DataBoundConstructor
+	public ConsuloInstallation(String name, String home, List<? extends ToolProperty<?>> properties)
+	{
+		super(name, home, properties);
+	}
+}
